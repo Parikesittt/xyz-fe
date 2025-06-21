38c331f8f8a50bb51d5b7738067bfd5d7f3ac505
Parikesit <152413959+Parikesittt@users.noreply.github.com>
2025-06-21 23:13:12 +0700

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'), // Updated plugin import
+    require('autoprefixer'),
+  ],
+};
---
+++ vite.config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
+import autoprefixer from 'autoprefixer'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [vue()],
+    base: './',
+    css: {
+      postcss: './postcss.config.js'
+    },
+    resolve: {
+      alias: [
+        // webpack path resolve to vitejs
+        {
+          find: /^~(.*)$/,
+          replacement: '$1',
+        },
+        {
+          find: '@/',
+          replacement: `${path.resolve(__dirname, 'src')}/`,
+        },
+        {
+          find: '@',
+          replacement: path.resolve(__dirname, '/src'),
+        },
+      ],
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue', '.scss'],
+    },
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true
+        }
+      },
+    },
+  }
+})
